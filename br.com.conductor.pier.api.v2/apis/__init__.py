f661543f9fdd1ee87af6effb77131890717a34d6
Filipe Germano <filipe.germano@conductor.com.br>
2016-11-29 17:17:45 -0300

    SPL0003 - Criando teste unitário para a rotina embossing e preparando versão para homologação

---
+++ br.com.conductor.pier.api.v2/apis/__init__.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import
+
+# import apis into api package
+from .base_api import BaseApi
+from .cartao_api import CartaoApi
+from .conta_api import ContaApi
+from .endereco_api import EnderecoApi
+from .estagio_cartao_api import EstagioCartaoApi
+from .origem_comercial_api import OrigemComercialApi
+from .pessoa_api import PessoaApi
+from .portador_api import PortadorApi
+from .produto_api import ProdutoApi
+from .status_cartao_api import StatusCartaoApi
+from .status_conta_api import StatusContaApi
+from .status_impressao_api import StatusImpressaoApi
+from .telefone_api import TelefoneApi
+from .tipo_endereco_api import TipoEnderecoApi
+from .tipo_telefone_api import TipoTelefoneApi
+from .token_api import TokenApi
