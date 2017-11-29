d1136865ffa07667138889a1e782397cc9f6c9a2
valber.paulino <valber.paulino@NJP0037>
2017-11-29 09:48:52 -0300

    versão: 2.46.5

--- br.com.conductor.pier.api.v2/api_client.py
+++ br.com.conductor.pier.api.v2/api_client.py
@@ -81,7 +81,7 @@ class ApiClient(object):
             self.host = host
         self.cookie = cookie
         # Set default User-Agent.
-        self.user_agent = 'Swagger-Codegen/2.46.4/python'
+        self.user_agent = 'Swagger-Codegen/2.46.5/python'
 
     @property
     def user_agent(self):
