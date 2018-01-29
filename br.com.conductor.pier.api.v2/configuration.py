d153775b1d76e1c2401cce3ea522d442b78b9d62
devconductor <thiago.sampaio@conductor.com.br>
2018-01-29 12:24:22 -0200

    Atualização SDK

--- br.com.conductor.pier.api.v2/configuration.py
+++ br.com.conductor.pier.api.v2/configuration.py
@@ -227,5 +227,5 @@ class Configuration(object):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 0.0.1\n"\
-               "SDK Package Version: 2.50.11".\
+               "SDK Package Version: 2.50.15".\
                format(env=sys.platform, pyversion=sys.version)
