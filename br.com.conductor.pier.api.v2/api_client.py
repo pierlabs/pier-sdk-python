8b61186dedf7a8bdd503b83fde7db666ce9c3ed6
devconductor <thiago.sampaio@conductor.com.br>
2018-02-20 17:00:10 -0300

    Atualização SDK

--- br.com.conductor.pier.api.v2/api_client.py
+++ br.com.conductor.pier.api.v2/api_client.py
@@ -81,7 +81,7 @@ class ApiClient(object):
             self.host = host
         self.cookie = cookie
         # Set default User-Agent.
-        self.user_agent = 'Swagger-Codegen/2.54.2/python'
+        self.user_agent = 'Swagger-Codegen/2.54.4/python'
 
     @property
     def user_agent(self):
